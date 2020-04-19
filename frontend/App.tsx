8547d7c2634fccfca5cf3b52ba170e901903cc81
Nikita Marinosyan <nikita.marinosyan@gmail.com>
2020-04-19 14:51:45 +0300

    Started implementing auth

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,12 +1,69 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import * as firebase from 'firebase';
+import { firebaseConfig } from './firebase.config';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component<any, any> {
+    state: any = {
+        loading: true,
+        user: null
+    }
+
+    storeHighScore(userId: string, score: number) {
+        firebase.database().ref('users/' + userId).set({
+            highscore: score
+        });
+    }
+
+    componentDidMount(): void {
+        firebase.initializeApp(firebaseConfig);
+
+        firebase.auth()
+            .createUserWithEmailAndPassword('nikita.marinosyan@gmail.com', 'SuperSecretPassword!')
+            .then(() => {
+                console.log('User account created & signed in!');
+            })
+            .catch(error => {
+                if (error.code === 'auth/email-already-in-use') {
+                    console.log('That email address is already in use!');
+                }
+
+                if (error.code === 'auth/invalid-email') {
+                    console.log('That email address is invalid!');
+                }
+
+                console.error(error);
+            });
+
+        firebase.auth().onAuthStateChanged(user => {
+            this.setState({
+                user,
+                loading: false
+            })
+        });
+    }
+
+    componentWillUnmount(): void {
+
+    }
+
+    render(): React.ReactNode {
+        return (
+            <View style={{ padding: 40 }}>
+                {this.state.loading && <Text>Loading</Text>}
+
+                {
+                    this.state.user
+                        ? <Text>{this?.state?.user?.email}</Text>
+                        : <Text>You are not authorized</Text>
+                }
+
+                <TouchableOpacity onPress={() => this.storeHighScore('n', 100)}>
+                    <Text>Hello</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
---
+++ frontend/firebase.config.ts
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase';
+
+// Optionally import the services that you want to use
+//import "firebase/auth";
+//import "firebase/database";
+//import "firebase/firestore";
+//import "firebase/functions";
+//import "firebase/storage";
+
+// Initialize Firebase
+export const firebaseConfig = {
+    apiKey: "AIzaSyC5tyioGXxNyOgE2DPfhUhW4ueGwEMRY1E",
+    authDomain: "i-dare-you-142ea.firebaseapp.com",
+    databaseURL: "https://i-dare-you-142ea.firebaseio.com",
+    projectId: "i-dare-you-142ea",
+    storageBucket: "i-dare-you-142ea.appspot.com",
+    messagingSenderId: "106861285226",
+    appId: "1:106861285226:web:f8db035e826aed2d9e5f13",
+    measurementId: "G-N5MQLNQXG1"
+};
