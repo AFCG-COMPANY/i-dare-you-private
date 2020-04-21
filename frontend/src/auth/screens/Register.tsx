354a9ebe568bfc7c548f17a548985423ba06f98a
Fedor <arbuzovfp@gmail.com>
2020-04-21 20:35:18 +0300

    add reset password

--- frontend/src/auth/screens/Register.tsx
+++ frontend/src/auth/screens/Register.tsx
@@ -62,10 +62,12 @@ export function Register({ navigation, route }: AuthNavProps<"Register">) {
       .catch((error) => {
         if (error.code === "auth/email-already-in-use") {
           console.log("That email address is already in use!");
+          dispatch({ type: ActionTypes.Error, payload: "That email address is already in use!" })
         }
 
         if (error.code === "auth/invalid-email") {
           console.log("That email address is invalid!");
+          dispatch({ type: ActionTypes.Error, payload: "That email address is invalid!"})
         }
 
         console.error(error);
