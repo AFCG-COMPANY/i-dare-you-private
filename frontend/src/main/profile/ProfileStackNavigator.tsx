43bec14b16d7f9ec721218841d309b76597c5d47
Nikita Marinosyan <nikita.marinosyan@gmail.com>
2020-04-26 16:15:40 +0300

    Added settings screen

--- frontend/src/main/profile/ProfileStackNavigator.tsx
+++ frontend/src/main/profile/ProfileStackNavigator.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Profile } from './Profile';
+import { Settings } from './Settings';
+import { Ionicons } from '@expo/vector-icons';
+import { Platform } from 'react-native';
+import { Button } from 'react-native-elements';
 
 export type ProfileParamList = {
     Profile: undefined;
@@ -16,6 +20,20 @@ export const ProfileStackNavigator: React.FC<ProfileStackProps> = ({}) => {
             <ProfileStack.Screen
                 name='Profile'
                 component={Profile}
+                options={({ navigation, route }) => ({
+                    headerRight: () => (
+                        <Button
+                            type='clear'
+                            containerStyle={{marginRight: 20}}
+                            icon={<Ionicons name={Platform.OS === 'ios' ? 'ios-settings' : 'md-settings'} size={20} />}
+                            onPress={() => navigation.navigate('Settings')}
+                        />
+                    )
+                })}
+            />
+            <ProfileStack.Screen
+                name='Settings'
+                component={Settings}
             />
         </ProfileStack.Navigator>
     );
