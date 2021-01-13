902dd259ba9b6ff0d86aad32f1332c9823daa8e9
Федор <fedor@MacBook-Air-Fedor.local>
2021-01-13 07:41:18 +0300

    fix

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,6 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Root from './src/Root';
+import { Platform } from 'react-native';
+import Constants from 'expo-constants';
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
+import { cos } from 'react-native-reanimated';
 
 export default function App() {
-    return <Root />
+    useEffect(() => {
+        notification();
+    }, []);
+
+    return <Root />;
 }
+
+const notification = async () => {
+    const token = await Notifications.getExpoPushTokenAsync();
+
+    alert(token);
+    Notifications.createChannelAndroidAsync('chat-messages', {
+        name: 'Chat messages',
+        sound: true
+    });
+
+    const { status: existingStatus } = await Permissions.getAsync(
+        Permissions.NOTIFICATIONS
+    );
+
+    let finalStatus = existingStatus;
+
+    if (existingStatus !== 'granted') {
+        const { status } = await Permissions.askAsync(
+            Permissions.NOTIFICATIONS
+        );
+        finalStatus = status;
+    }
+
+    if (finalStatus !== 'granted') {
+        return;
+    }
+
+    await persistToken();
+};
+
+const persistToken = async () => {
+    try {
+        const token = await Notifications.getExpoPushTokenAsync();
+
+        console.log(token);
+    } catch (err) {
+        throw err;
+    }
+};
